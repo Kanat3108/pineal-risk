71301333360cfc76107f3c2d47f8d5723d45686d
kanat3108 <kanat3108@gmail.com>
2019-06-26 14:44:00 +0200

    changed file updates

--- pineal-risk.php
+++ pineal-risk.php
@@ -95,21 +95,19 @@ function wptuts_add_color_picker( $hook ) {
         wp_enqueue_script( 'custom-script-handle', plugins_url( 'includes/js/custom-script.js', __FILE__ ), array( 'wp-color-picker' ), false, true ); 
     }
 }
-/*add_filter('admin_head','ShowTinyMCE');
-function ShowTinyMCE() {
-	// conditions here
-	wp_enqueue_script( 'common' );
-	wp_enqueue_script( 'jquery-color' );
-	wp_print_scripts('editor');
-	if (function_exists('add_thickbox')) add_thickbox();
-	wp_print_scripts('media-upload');
-	if (function_exists('wp_tiny_mce')) wp_tiny_mce();
-	wp_admin_css();
-	wp_enqueue_script('utils');
-	do_action("admin_print_styles-post-php");
-	do_action('admin_print_styles');
 
-}*/
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/Kanat3108/pineal-risk.git',
+	__FILE__,
+	'pineal-risk'
+);
+
+//Optional: If you're using a private repository, specify the access token like this:
+//$myUpdateChecker->setAuthentication('your-token-here');
+
+//Optional: Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('master');
 
 
 
